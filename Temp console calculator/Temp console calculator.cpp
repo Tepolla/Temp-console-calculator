13e5f277f8d59834302ad413ed900223c25edf28
Derek <dpt5349@psu.edu>
2023-05-18 01:03:38 -0400

    Error handling added

--- Temp console calculator/Temp console calculator.cpp
+++ Temp console calculator/Temp console calculator.cpp
@@ -1,21 +1,42 @@
-// Temp console calculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "tempConversion.h"
+#include <limits>
 using namespace std;
+
+long double valueVerification();
+char characterVerification();
 
 int main()
 {
     long double startingValue = 0.0;
     char startingUnit;
 
-    cout << "Please enter in a value: ";
-    cin >> startingValue;
-    cout << "Please enter in the units you would like to start with (\370F, \370C, \370K): ";
-    cin >> startingUnit;
-    tempConversion TC(startingUnit, startingValue);
+    
+    tempConversion TC(characterVerification(), valueVerification());
     cout << endl << TC.getFahrenheit() << endl << TC.getCelsius() << endl << TC.getKelvin();
 
     return 0;
+}
+long double valueVerification() {
+    long double startingValue;
+
+    cout << "Please enter a value: ";
+    while (!(cin >> startingValue)) {
+        cout << "Please enter a value: ";
+        cin.clear();  // Clear the fail state
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore the remaining input
+    }
+
+    return startingValue;
+}
+
+char characterVerification() {
+    char startingUnit = 'L';
+     do {
+        cout << "Please enter in the units you would like to start with (\370F, \370C, \370K): ";
+        cin >> startingUnit;
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore the remaining input
+        startingUnit = toupper(startingUnit);
+     } while (startingUnit != 'F' && startingUnit != 'C' && startingUnit != 'K');
+    return startingUnit;
 }
