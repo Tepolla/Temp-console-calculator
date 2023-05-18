aa4f2842d26cbe6fb98b3c3cc903542d399c48a4
Derek <dpt5349@psu.edu>
2023-05-18 01:06:00 -0400

    Removing some useless items

--- Temp console calculator/Temp console calculator.cpp
+++ Temp console calculator/Temp console calculator.cpp
@@ -8,30 +8,24 @@ char characterVerification();
 
 int main()
 {
-    long double startingValue = 0.0;
-    char startingUnit;
-
-    
     tempConversion TC(characterVerification(), valueVerification());
     cout << endl << TC.getFahrenheit() << endl << TC.getCelsius() << endl << TC.getKelvin();
-
     return 0;
 }
+
 long double valueVerification() {
     long double startingValue;
-
     cout << "Please enter a value: ";
     while (!(cin >> startingValue)) {
         cout << "Please enter a value: ";
         cin.clear();  // Clear the fail state
         cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore the remaining input
     }
-
     return startingValue;
 }
 
 char characterVerification() {
-    char startingUnit = 'L';
+    char startingUnit;
      do {
         cout << "Please enter in the units you would like to start with (\370F, \370C, \370K): ";
         cin >> startingUnit;
